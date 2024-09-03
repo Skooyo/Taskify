a834d6876da755a79bb97442c7c1401760a33ad6
Vincent Wesley Liem <vinc0005@student.monash.edu>
2024-09-03 19:08:31 +0800

    Feature: Add mongodb database connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { getAllProductBacklogItems } from "@/lib/actions/product_backlog.actions";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const a = await getAllProductBacklogItems();
+      console.log(a);
+    }
+    
+    fetchData();
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
