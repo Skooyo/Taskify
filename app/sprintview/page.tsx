9c74bf3e8c1e3764086ee241d2bca10498cacd11
Vincent Wesley Liem <vinc0005@student.monash.edu>
2024-09-16 19:50:04 +0800

    Feature - Create routing for sprint view page in app/sprintview/page.tsx

--- app/sprintview/page.tsx
+++ app/sprintview/page.tsx
@@ -1,9 +1,94 @@
+import SprintCard from "@/components/SprintCard";
+import { IProductBacklogItem } from "@/lib/database/models/product_backlog_item.model";
+import { ISprint } from "@/lib/database/models/sprint.model";
 import React from "react";
 
-const mockData = [{}];
+const mockData = [
+  {
+    _id: "1",
+    title: "Sprint 1",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+  {
+    _id: "2",
+    title: "Sprint 2",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+  {
+    _id: "3",
+    title: "Sprint 3",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+  {
+    _id: "4",
+    title: "Sprint 4",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+  {
+    _id: "5",
+    title: "Sprint 5",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+  {
+    _id: "6",
+    title: "Sprint 6",
+    status: "Active",
+    startDate: new Date(),
+    endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+    createdAt: new Date(),
+    notStartedTasks: [] as IProductBacklogItem[],
+    inProgressTasks: [] as IProductBacklogItem[],
+    completedTasks: [] as IProductBacklogItem[],
+  },
+] as ISprint[];
 
 const SprintView = () => {
-  return <div>SprintView</div>;
+  return (
+    <div className="flex flex-col mt-[70px] mx-4 gap-6">
+      <div>
+        <h1 className="text-4xl font-semibold ml-10">Sprints</h1>
+      </div>
+      <div className="flex flex-col gap-10">
+        {mockData.map((sprint) => {
+          return (
+            <div key={sprint._id} className="bg-red min-w-full">
+              <SprintCard />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SprintView;
