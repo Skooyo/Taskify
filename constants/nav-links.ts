b6f89456e07d1db1295e0f492b68f579de24bc98
pine0002 <pine0002@student.monash.edu>
2024-09-29 21:59:31 +0800

    Feature - Navbar icons resize automatically

--- constants/nav-links.ts
+++ constants/nav-links.ts
@@ -1,18 +1,26 @@
+import { BiTask } from "react-icons/bi";
+import { BiSolidDashboard } from "react-icons/bi";
+import { TbFileStack } from "react-icons/tb";
+import { FaUser, FaUserTie } from "react-icons/fa6";
 export const links = [
   {
     name: "Product Backlog",
     href: "/",
+    icon: BiTask
   },
   {
     name: "Sprint Board",
     href: "/sprints",
+    icon: BiSolidDashboard 
   },
   {
     name: "Team Dashboard",
     href: "/team",
+    icon: FaUser  
   },
   {
     name: "Admin Dashboard",
     href: "/admin",
+    icon: FaUserTie
   },
 ];
