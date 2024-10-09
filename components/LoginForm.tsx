c4b394b52ba5d9b9b2049239ed7a3ffb794eb0ec
JhinYen <jlow0050@student.monash.edu>
2024-10-10 02:38:21 +0800

    Feature - [US13] Add a username input field to our LoginForm

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginForm: React.FC = () => {
+  const form = useForm();
+
   return (
     <form>
-      {/* our fields and buttons should go here later */}
+      {/* Username Field */}
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username:</FormLabel>
+            <FormControl>
+              <Input
+                {...field}
+                className="border-black rounded-lg"
+                placeholder="Enter your username"
+                onChange={(e) => {
+                  field.onChange(e);
+                }}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
     </form>
   );
 };
